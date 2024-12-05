974acfae26ab55de2c5785e472e921788277aa84
ortenszky <ortenszkytomi@gmail.com>
2024-12-05 21:19:26 +0100

    Update db.py

--- db.py
+++ db.py
@@ -1,10 +1,9 @@
 import sqlite3
-from habit import Habit
 
 
 def get_db():
     """
-        Initialize and return the database connection, creating tables if they do not exist.
+        Initialize and return the database connection, creating the tables habits and counters if they do not exist.
 
         Returns:
         -------
@@ -36,7 +35,7 @@ def get_db():
 
 def get_habits_list(db):
     """
-       Retrieve a list of all habit names from the database.
+       Retrieve a list of all the habits from the database.
 
        Parameters:
        ----------
@@ -62,7 +61,7 @@ def get_habits_by_periodicity(db, periodicity):
     db : sqlite3.Connection
         The database connection object.
     periodicity : str
-        The periodicity to filter by.
+        The periodicity to filter by. (daily or weekly)
 
     Returns:
     -------
@@ -70,7 +69,7 @@ def get_habits_by_periodicity(db, periodicity):
         A list of habit names matching the given periodicity.
     """
     cursor = db.cursor()
-    cursor.execute('SELECT name habits WHERE periodicity = ?', (periodicity,))
+    cursor.execute('SELECT name FROM habits WHERE periodicity = ?', (periodicity,))
     return[row[0]for row in cursor.fetchall()]
 
 
@@ -91,14 +90,14 @@ def get_counter(db, name):
            A Counter object if the habit exists, otherwise None.
     """
     cursor = db.cursor()
-    cursor.execute('SELECT * FROM habits WHERE name = ?', (name,))
+    cursor.execute('SELECT id FROM habits WHERE name = ?', (name,))
     habit = cursor.fetchone()
-    if habit:
-        return Habit(name=habit[1], description=habit[2], periodicity=habit[3], id=habit[0])
-    else:
-        return None
+    if not habit:
+        return 0
 
+    habit_id = habit[0]
 
+    cursor.execute('SELECT COUNT(*) FROM counters WHERE habit_id = ?', (habit_id,))
+    count = cursor.fetchone()[0]
 
-
-
+    return count
